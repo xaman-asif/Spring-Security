273a5f6eff7a50218d77c3dd7da96a5d4a1828e3
Asif Jaman <asifuzzaman.opi@gmail.com>
2023-07-11 23:05:41 +0600

    Understanding CORs & CSRF

--- 6 - Understanding CORs & CSRF/src/main/java/com/eazybytes/repository/ContactRepository.java
+++ 6 - Understanding CORs & CSRF/src/main/java/com/eazybytes/repository/ContactRepository.java
@@ -1,2 +1,12 @@
-package com.eazybytes.repository;public class ContactRepository {
+package com.eazybytes.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.eazybytes.model.Contact;
+
+@Repository
+public interface ContactRepository extends CrudRepository<Contact, Long> {
+	
+	
 }
