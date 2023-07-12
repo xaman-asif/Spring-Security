d5c0995219997c6f4eaf37b70dd2eb39264f860f
Asif Jaman <asifuzzaman.opi@gmail.com>
2023-07-12 16:21:32 +0600

    bank-app-ui

--- 6 - Understanding CORs & CSRF/src/main/resources/sql/scripts.sql
+++ 6 - Understanding CORs & CSRF/src/main/resources/sql/scripts.sql
@@ -18,19 +18,19 @@ CREATE TABLE `authorities` (
 INSERT IGNORE INTO `users` VALUES (NULL, 'happy', '12345', '1');
 INSERT IGNORE INTO `authorities` VALUES (NULL, 'happy', 'write');
 
-CREATE TABLE `customer` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `email` varchar(45) NOT NULL,
-  `pwd` varchar(200) NOT NULL,
-  `role` varchar(45) NOT NULL,
-  PRIMARY KEY (`id`)
-);
-
-INSERT INTO `customer` (`email`, `pwd`, `role`)
- VALUES ('johndoe@example.com', '54321', 'admin');
- 
-INSERT INTO `customer` (`email`, `pwd`, `role`)
- VALUES ('happy@example.com', '$2y$12$oRRbkNfwuR8ug4MlzH5FOeui.//1mkd.RsOAJMbykTSupVy.x/vb2', 'admin');
+# CREATE TABLE `customer` (
+#   `id` int NOT NULL AUTO_INCREMENT,
+#   `email` varchar(45) NOT NULL,
+#   `pwd` varchar(200) NOT NULL,
+#   `role` varchar(45) NOT NULL,
+#   PRIMARY KEY (`id`)
+# );
+#
+# INSERT INTO `customer` (`email`, `pwd`, `role`)
+#  VALUES ('johndoe@example.com', '54321', 'admin');
+#
+# INSERT INTO `customer` (`email`, `pwd`, `role`)
+#  VALUES ('happy@example.com', '$2y$12$oRRbkNfwuR8ug4MlzH5FOeui.//1mkd.RsOAJMbykTSupVy.x/vb2', 'admin');
  
 CREATE TABLE `customer` (
   `customer_id` int NOT NULL AUTO_INCREMENT,
@@ -58,7 +58,7 @@ CREATE TABLE `accounts` (
 );
 
 INSERT INTO `accounts` (`customer_id`, `account_number`, `account_type`, `branch_address`, `create_dt`)
- VALUES (1, 186576453434, 'Savings', '123 Main Street, New York', CURDATE());
+ VALUES (1, 1865764534, 'Savings', '123 Main Street, New York', CURDATE());
  
 CREATE TABLE `account_transactions` (
   `transaction_id` varchar(200) NOT NULL,
@@ -80,22 +80,22 @@ CREATE TABLE `account_transactions` (
 
  
 INSERT INTO `account_transactions` (`transaction_id`, `account_number`, `customer_id`, `transaction_dt`, `transaction_summary`, `transaction_type`,`transaction_amt`, 
-`closing_balance`, `create_dt`)  VALUES (UUID(), 186576453434, 1, CURDATE()-7, 'Coffee Shop', 'Withdrawal', 30,34500,CURDATE()-7);
+`closing_balance`, `create_dt`)  VALUES (UUID(), 1865764534, 1, CURDATE()-7, 'Coffee Shop', 'Withdrawal', 30,34500,CURDATE()-7);
 
 INSERT INTO `account_transactions` (`transaction_id`, `account_number`, `customer_id`, `transaction_dt`, `transaction_summary`, `transaction_type`,`transaction_amt`, 
-`closing_balance`, `create_dt`)  VALUES (UUID(), 186576453434, 1, CURDATE()-6, 'Uber', 'Withdrawal', 100,34400,CURDATE()-6);
+`closing_balance`, `create_dt`)  VALUES (UUID(), 1865764534, 1, CURDATE()-6, 'Uber', 'Withdrawal', 100,34400,CURDATE()-6);
 
 INSERT INTO `account_transactions` (`transaction_id`, `account_number`, `customer_id`, `transaction_dt`, `transaction_summary`, `transaction_type`,`transaction_amt`, 
-`closing_balance`, `create_dt`)  VALUES (UUID(), 186576453434, 1, CURDATE()-5, 'Self Deposit', 'Deposit', 500,34900,CURDATE()-5);
+`closing_balance`, `create_dt`)  VALUES (UUID(), 1865764534, 1, CURDATE()-5, 'Self Deposit', 'Deposit', 500,34900,CURDATE()-5);
 
 INSERT INTO `account_transactions` (`transaction_id`, `account_number`, `customer_id`, `transaction_dt`, `transaction_summary`, `transaction_type`,`transaction_amt`, 
-`closing_balance`, `create_dt`)  VALUES (UUID(), 186576453434, 1, CURDATE()-4, 'Ebay', 'Withdrawal', 600,34300,CURDATE()-4);
+`closing_balance`, `create_dt`)  VALUES (UUID(), 1865764534, 1, CURDATE()-4, 'Ebay', 'Withdrawal', 600,34300,CURDATE()-4);
 
 INSERT INTO `account_transactions` (`transaction_id`, `account_number`, `customer_id`, `transaction_dt`, `transaction_summary`, `transaction_type`,`transaction_amt`, 
-`closing_balance`, `create_dt`)  VALUES (UUID(), 186576453434, 1, CURDATE()-2, 'OnlineTransfer', 'Deposit', 700,35000,CURDATE()-2);
+`closing_balance`, `create_dt`)  VALUES (UUID(), 1865764534, 1, CURDATE()-2, 'OnlineTransfer', 'Deposit', 700,35000,CURDATE()-2);
 
 INSERT INTO `account_transactions` (`transaction_id`, `account_number`, `customer_id`, `transaction_dt`, `transaction_summary`, `transaction_type`,`transaction_amt`, 
-`closing_balance`, `create_dt`)  VALUES (UUID(), 186576453434, 1, CURDATE()-1, 'Amazon.com', 'Withdrawal', 100,34900,CURDATE()-1);
+`closing_balance`, `create_dt`)  VALUES (UUID(), 1865764534, 1, CURDATE()-1, 'Amazon.com', 'Withdrawal', 100,34900,CURDATE()-1);
 
 
 CREATE TABLE `loans` (
